29c2d56b55b5b4d1be3172b3ae9e0f6266b40ace
John <john.saygnavong@revisionz.com>
2021-02-04 10:55:32 -0700

    removed a bunch of debug files.
    installed some nuget packages for admob
    beginning adding ad support

--- unitConverter.Android/MainActivity.cs
+++ unitConverter.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Ads;
 
 namespace unitConverter.Droid
 {
@@ -18,7 +19,8 @@ namespace unitConverter.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            //The Ad ID used here is a sample ad
+            MobileAds.Initialize(ApplicationContext, "ca-app-pub-3940256099942544/6300978111");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
